8a0c5fde8b126ac91ef32756172dda5159801da5
Gustavo Barcario <gustavobarcario.0788@gmail.com>
2023-08-13 23:12:04 -0300

    Inicio de desenvolvimento APP Help Desk

---
+++ valida_login.php
@@ -0,0 +1,64 @@
+<?php
+    session_start();
+
+    $_SESSION['x']='oi sou um valor de sessão';
+    print_r($_SESSION);
+    echo '<hr/>';
+    echo $_SESSION['y'] .'<br/>';
+
+    //varivael que verifica se a autenticacao foi realizada
+    $usuario_autenticado = false;
+    
+    //usuarios do sistema
+    $usuarios_app = array(
+      array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+      array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+    );
+
+    foreach ($usuarios_app as $key => $user) {
+
+      /*
+      echo 'Usuário APP: ' . $user['email'] . ' / ' . $user['senha'];
+      echo '<br/>';
+      echo 'Usuário FORM: ' . $_POST['email'] . ' / ' . $_POST['senha'];
+      echo '<hr/>';
+      */
+
+      if (($user['email'] == $_POST['email']) && ($user['senha'] == $_POST['senha'])){
+        $usuario_autenticado = true;
+      }
+    }
+
+    if ($usuario_autenticado) {
+      echo "Usuário autenticado";
+    }else {
+      header('Location: index.php?login=erro');
+      //echo "Erro na autenticação do usuário";
+    }
+
+    /*
+    echo '<pre>';
+    print_r($usuarios_app);
+    echo '</pre>';
+    */
+
+    /*
+    print_r($_GET);
+
+    echo '<br/>';
+
+    echo $_GET['email'];
+    echo '<br/>';
+    echo $_GET['senha'];
+    */
+
+    /*
+    print_r($_POST);
+
+    echo '<br/>';
+
+    echo $_POST['email'];
+    echo '<br/>';
+    echo $_POST['senha'];
+    */
+?>
