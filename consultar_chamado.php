1c5fb35e4a2dfc9b666fc4afb834c2b6e748738c
Gustavo Barcario <gustavobarcario.0788@gmail.com>
2023-08-15 23:59:03 -0300

    Aplicando controle de perfil de usuários

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,7 +1,4 @@
-<?php 
-require_once "validador_acesso.php";
-  
-?>
+<? require_once "validador_acesso.php"; ?>
 
 <?php
 
@@ -71,15 +68,23 @@ require_once "validador_acesso.php";
               <?php
                 $chamado_dados = explode('#', $chamado);
 
+                if ($_SESSION['perfil_id'] == 2) {
+                  //Exibir apenas se for exibido pelo usuário
+                  if ($_SESSION['id'] != $chamado_dados[0]) {
+                    continue;
+                  }
+
+                }
+
                 if(count($chamado_dados) < 3){
                   continue;
                 }
               ?>
               <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title"><?= $chamado_dados[0]?></h5>
-                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1]?></h6>
-                  <p class="card-text"><?= $chamado_dados[2]?></p>
+                  <h5 class="card-title"><?= $chamado_dados[1]?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[2]?></h6>
+                  <p class="card-text"><?= $chamado_dados[3]?></p>
 
                 </div>
               </div>
