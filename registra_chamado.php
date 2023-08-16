cf30e97d70edaa7acc01ba9e5b9978b277d30eb3
Gustavo Barcario <gustavobarcario.0788@gmail.com>
2023-08-15 23:16:05 -0300

    Corrige erros de telas registra e consulta chamado

--- registra_chamado.php
+++ registra_chamado.php
@@ -1,36 +1,29 @@
-
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Ler o número do último chamado do arquivo (se existir)
-  $ultimoChamado = 0;
-  if (file_exists('ultimo_chamado.txt')) {
-      $ultimoChamado = (int) file_get_contents('ultimo_chamado.txt');
-  }
-  
-  // Incrementar o número do chamado
-  $novoChamado = $ultimoChamado + 1;
 
-  // Trabalhando na montagem do texto
-  $titulo = str_replace('#', '-', $_POST['titulo']);
-  $categoria = str_replace('#', '-', $_POST['categoria']);
-  $descricao = str_replace('#', '-', $_POST['descricao']);
+    echo '<pre>';
 
-  // Montando o texto com melhor formatação, incluindo o número do chamado
-  $texto = "Chamado #$novoChamado\nTítulo: $titulo\nCategoria: $categoria\nDescrição: $descricao\n\n";
+    print_r($_POST);
 
-  // Abrindo o arquivo
-  $arquivo = fopen('arquivo.txt', 'a');
+    echo '</pre>';
 
-  // Escrevendo texto
-  fwrite($arquivo, $texto);
+    //Montando o texto
+    $titulo = str_replace('#','-',$_POST['titulo']);
+    $categoria = str_replace('#','-',$_POST['categoria']);
+    $descricao = str_replace('#','-',$_POST['descricao']);
+    //implode('#',$_POST);
 
-  // Atualizando o número do último chamado
-  file_put_contents('ultimo_chamado.txt', $novoChamado);
+    $texto = $titulo.'#'.$categoria.'#'.$descricao.PHP_EOL;
+    
+    //http://php.net/manual/pt_BR/function.fopen.php
+    //Abrindo o arquivo
+    $arquivo = fopen('arquivo.hd','a');
 
-  // Fechando o arquivo
-  fclose($arquivo);
+    //Escrevendo texto no arquivo
+    fwrite($arquivo,$texto);
 
-  header('Location: abrir_chamado.php');
-}
+    //Fechando o arquivo
+    fclose($arquivo);
 
+    //echo $texto;
+    header('Location: abrir_chamado.php')
 ?>
